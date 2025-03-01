9dc7bb6e9d8c53507dbbef943756bb7d3a2349f5
mrndstvndv <stvndvmrnd@gmail.com>
2025-03-01 09:55:50 +0800

    just saving this

--- src/app/product/product.page.ts
+++ src/app/product/product.page.ts
@@ -1,8 +1,83 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 import { IonBackButton, IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonTitle, IonToolbar } from "@ionic/angular/standalone";
 import { addIcons } from "ionicons";
 import { cartOutline, heartOutline } from "ionicons/icons";
+
+interface Product {
+  id: number
+  title: string
+  category: string
+  size: string
+  price: number
+  rating: number
+  image: string
+  isFavorite: boolean
+}
+
+let products: Product[] = [
+  {
+    id: 1,
+    title: 'Cotton Baggy Pants',
+    category: 'UNISEX',
+    size: 'S-XL',
+    price: 790,
+    rating: 5.0,
+    image: 'assets/images/cotton-baggy-pants.png',
+    isFavorite: false
+  },
+  {
+    id: 2,
+    title: 'AIRism Cotton Crew Neck Long Sleeve T-Shirt',
+    category: 'UNISEX',
+    size: 'S-XL',
+    price: 990,
+    rating: 5.0,
+    image: 'assets/images/airism-cotton-tshirt.png',
+    isFavorite: false
+  },
+  {
+    id: 3,
+    title: 'Cotton T-Shirt',
+    category: 'MENS',
+    size: 'S-XL',
+    price: 690,
+    rating: 3.0,
+    image: 'assets/images/cotton-tshirt.png',
+    isFavorite: false
+  },
+  {
+    id: 4,
+    title: "WOMEN'S SMART ANKLE PANTS 2WAY STRETCH",
+    category: 'WOMENS',
+    size: 'S-XL',
+    price: 990,
+    rating: 4.0,
+    image: 'assets/images/womens-smart-ankle-pants.png',
+    isFavorite: false
+  },
+  {
+    id: 5,
+    title: "WOMEN'S U CREW NECK SHORT SLEEVE T-SHIRT",
+    category: 'WOMENS',
+    size: 'S-XL',
+    price: 780,
+    rating: 5.0,
+    image: 'assets/images/WOMENSUCREWNECKSHORTSLEEVET-SHIRT.png',
+    isFavorite: false
+  },
+  {
+    id: 6,
+    title: "Rayon Skipper Collar 3/4 Sleeve Blouse",
+    category: 'WOMENS',
+    size: 'S-L',
+    price: 990,
+    rating: 5.0,
+    image: 'assets/images/RayonSkipperCollarSleeveBlouse.png',
+    isFavorite: false
+  }
+];
 
 @Component({
   selector: 'app-product',
@@ -11,27 +86,24 @@ import { cartOutline, heartOutline } from "ionicons/icons";
   imports: [IonHeader, IonToolbar, IonButtons, IonButton, IonBackButton, IonTitle, IonIcon, IonContent, CommonModule]
 })
 export class ProductPage {
-  product = {
-    name: "AIRism Cotton Crew Neck Long Sleeve T-Shirt",
-    price: 250,
-    description:
-      "Your personal style, perfected. This AIRism Cotton Crew Neck Long Sleeve T-Shirt combines comfort and style for everyday wear.",
-    sizes: ["XS", "S", "M", "L", "XL"],
-    colors: ["White", "Black", "Navy", "Gray"],
-    rating: 4.5,
-    reviewCount: 128,
-    images: [
-      "assets/images/cotton-tshirt.png",
-      "/placeholder.svg?height=500&width=400",
-      "/placeholder.svg?height=500&width=400",
-    ],
-  }
+  product: Product = products[0]
 
-  constructor() {
+  constructor(private router: ActivatedRoute) {
     addIcons({
       'star': "assets/icon/star-rating-filled.svg",
       heartOutline,
       cartOutline
+    })
+  }
+
+  ngOnInit() {
+    this.router.queryParams.subscribe(params => {
+      const p = params as { id: number }
+      const product = products.find(product => product.id == p.id)
+
+      if (product != undefined) {
+        this.product = product
+      }
     })
   }
 }
